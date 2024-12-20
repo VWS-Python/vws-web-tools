6ac459dab77c9b73fbf16499c10c8ea356f8c5d9
Adam Dangoor <adamdangoor@gmail.com>
2024-12-20 09:52:10 +0000

    Move function to top level

--- src/my_mypy_plugin/mypy_plugin.py
+++ src/my_mypy_plugin/mypy_plugin.py
@@ -7,34 +7,35 @@ from mypy.plugin import FunctionSigContext, Plugin
 from mypy.types import CallableType
 
 
+def _transform_function_signature(
+    ctx: FunctionSigContext,
+) -> CallableType:
+    original_sig: CallableType = ctx.default_signature
+    new_arg_kinds: list[ArgKind] = []
+
+    for kind, name in zip(
+        original_sig.arg_kinds, original_sig.arg_names, strict=False
+    ):
+        # If name is None, it's a positional-only argument; leave it as is
+        if name is None:
+            new_arg_kinds.append(kind)
+        # Transform positional arguments that can also be keyword arguments
+        elif kind == ARG_POS:
+            new_arg_kinds.append(ARG_NAMED)
+        elif kind == ARG_OPT:
+            new_arg_kinds.append(ARG_NAMED_OPT)
+        else:
+            new_arg_kinds.append(kind)
+
+    return original_sig.copy_modified(arg_kinds=new_arg_kinds)
+
+
 class _KeywordOnlyPlugin(Plugin):
     def get_function_signature_hook(
         self,
         fullname: str,
     ) -> Callable[[FunctionSigContext], CallableType] | None:
-        def transform_function_signature(
-            ctx: FunctionSigContext,
-        ) -> CallableType:
-            original_sig: CallableType = ctx.default_signature
-            new_arg_kinds: list[ArgKind] = []
-
-            for kind, name in zip(
-                original_sig.arg_kinds, original_sig.arg_names, strict=False
-            ):
-                # If name is None, it's a positional-only argument; leave it as is
-                if name is None:
-                    new_arg_kinds.append(kind)
-                # Transform positional arguments that can also be keyword arguments
-                elif kind == ARG_POS:
-                    new_arg_kinds.append(ARG_NAMED)
-                elif kind == ARG_OPT:
-                    new_arg_kinds.append(ARG_NAMED_OPT)
-                else:
-                    new_arg_kinds.append(kind)
-
-            return original_sig.copy_modified(arg_kinds=new_arg_kinds)
-
-        return transform_function_signature
+        return _transform_function_signature
 
 
 def plugin(version: str) -> type[_KeywordOnlyPlugin]:
