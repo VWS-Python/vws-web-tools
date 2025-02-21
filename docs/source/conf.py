c0772a71448a14ab8d447753b9ca2c36f57a5117
Adam Dangoor <adamdangoor@gmail.com>
2025-02-21 12:42:59 +0000

    Remove outdated linkcheck ignore

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,10 +71,6 @@ html_theme_options = {
 
 # Retry link checking to avoid transient network errors.
 linkcheck_retries = 5
-linkcheck_ignore = [
-    # Requires login.
-    r"https://developer.vuforia.com/targetmanager",
-]
 
 spelling_word_list_filename = "../../spelling_private_dict.txt"
 
