50a6b5d585b35e644cfa81c83756450f299156fb
Adam Dangoor <adamdangoor@gmail.com>
2026-04-19 09:55:39 +0100

    Use pytest-beartype-tests plugin for test beartype
    Replace manual pytest_collection_modifyitems hooks with the
    https://github.com/adamtheturtle/pytest-beartype-tests dev dependency.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,6 @@
 import os
 
 import pytest
-from beartype import beartype
 
 from tests.credentials import VWSCredentials
 
@@ -15,16 +14,3 @@ def fixture_vws_credentials() -> VWSCredentials:
         email_address=os.environ["VWS_EMAIL_ADDRESS"],
         password=os.environ["VWS_PASSWORD"],
     )
-
-
-@beartype
-def pytest_collection_modifyitems(
-    session: pytest.Session,
-    config: pytest.Config,
-    items: list[pytest.Item],
-) -> None:
-    """Apply the beartype decorator to all collected test functions."""
-    del session, config
-    for item in items:
-        assert isinstance(item, pytest.Function)
-        item.obj = beartype(obj=item.obj)
